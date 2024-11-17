ae9681f709bfd46d085003ee98a7be53e98dff1e
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-11-17 17:39:40 +0200

    add static

--- project/settings.py
+++ project/settings.py
@@ -137,3 +137,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 AUTHENTICATION_BACKENDS=['accounts.backend.Login_Email_Username']
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'mohamedelazab2017@gmail.com'
+# EMAIL_HOST_PASSWORD = 'EMAIL_HOST_PASSWORD'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+
+LOGIN_REDIRECT_URL = '/'
